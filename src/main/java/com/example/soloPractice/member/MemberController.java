5c16a89381b1073d69e30904ee5bec7a5943c387
yj <uyj9011@naver.com>
2023-01-23 01:43:40 +0900

    ResponseEntity

--- src/main/java/com/example/soloPractice/member/MemberController.java
+++ src/main/java/com/example/soloPractice/member/MemberController.java
@@ -1,39 +1,39 @@
 package com.example.soloPractice.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController //해당 클래스가 REST API의 리소스를 처리하기 위한 API 엔드포인트로 동작함을 정의
-@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE}) //클라이언트의 요청과 클라이언트의 요청을 처리하는 핸들러 메서드를 매핑해줌
+@RequestMapping("/v1/members") //클라이언트의 요청과 클라이언트의 요청을 처리하는 핸들러 메서드를 매핑해줌
 public class MemberController {
     @PostMapping
-    public String postMember(@RequestParam("email")String  email,
+    public ResponseEntity postMember(@RequestParam("email")String  email,
                              @RequestParam("name")String name,
                              @RequestParam("phone")String phone) {
-        System.out.println("# email:" + email);
-        System.out.println("# name:" + name);
-        System.out.println("# phone" + phone);
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
 
-        String response =
-                "{\"" +
-                        "email\":\""+email+"\"," +
-                        "\"name\":\""+name+"\",\"" +
-                        "phone\":\"" + phone+
-                        "\"}";
-        return response;
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @GetMapping("/{member-id}")
-    public String getMember(@PathVariable("member-id") long memberId) {
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId:" + memberId);
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getMembers() {
+    public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
